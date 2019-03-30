904d9cee98ee60389d2f13918a67d17c58ed4e90
LOH Yu Jin <yujin.loh.2017@smu.edu.sg>
2019-03-31 02:39:47 +0800

    tweet

--- JQuery/get_tweet.php
+++ JQuery/get_tweet.php
@@ -3,10 +3,7 @@
 require_once('TwitterAPIExchange.php');
 
 $settings = array(
-    'oauth_access_token' => "1107643411438305285-xplsDik3gGWTS7ABpR3WkO26QAB7VJ",
-    'oauth_access_token_secret' => "WIeeb6S5P3mFtpClT1sJ6wR3hrl9ufdoTxWFM0C2YI0AN",
-    'consumer_key' => "Qv7fBJLsw7RUd7VVWHr2IwEC9",
-    'consumer_secret' => "U0UL4yXP1ONPc3c4CBShctyY7lFWtntaYSncLc8XjRzRDBppIn"
+// add keys
 );
 
 $url = 'https://api.twitter.com/1.1/search/tweets.json';
